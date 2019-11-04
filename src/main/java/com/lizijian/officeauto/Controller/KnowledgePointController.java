db18ace545270ae3117a76726a55799ebde52fd8
lizijian <camskywalker@outlook.com>
2019-11-04 22:46:28 +0800

    lizijian

--- src/main/java/com/lizijian/officeauto/Controller/KnowledgePointController.java
+++ src/main/java/com/lizijian/officeauto/Controller/KnowledgePointController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/knowlegepoints")
+@RequestMapping("/knowledgepoints")
 public class KnowledgePointController {
 
     @Autowired
